b54f636987af7de76cae82244a0a3ea981d07d41
RanjanGitHubb <ranjanbera1112@gmail.com>
2024-12-04 13:43:58 +0530

    update jenkins file

---
+++ service-cd.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+    stages {
+        stage('Repo clone') {
+            steps {
+                echo 'This is build stage'
+                sh 'rm -rf *'
+                sh 'git clone -b maven https://github.com/madandevops11/myfirstrepo.git'
+				  }
+			}
+		    stage ('Build'){
+                steps {
+                    echo 'This is validate stage'
+                    sh 'pwd'
+                    sh 'cd myfirstrepo/my-app && mvn clean && mvn package'
+		        }
+		    }
+            stage ('Test'){
+                steps {
+                    sh 'cd myfirstrepo/my-app && java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App'
+                }  
+		    }
+            stage('Docker build') {
+                steps {
+                    sh 'git clone -b master https://github.com/rbera04/docker.git'
+                    sh 'cp java.dockerfile myfirstrepo/my-app/'
+                    sh 'cd myfirstrepo/my-app && docker build -t myapp:v1 .'
+                }
+            }
+            stage('deploy'){
+                steps {
+                    sh 'docker run -dit -p 8085:80 myapp:v1'
+                }
+            }
+		}
+	}

6a57589ed22ed424aaf9137dc87609271292d012
RanjanGitHubb <ranjanbera1112@gmail.com>
2024-12-04 13:49:34 +0530

    update jenkins file

--- service-cd.groovy
+++ service-cd.groovy
@@ -20,15 +20,17 @@ pipeline {
                     sh 'cd myfirstrepo/my-app && java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App'
                 }  
 		    }
-            stage('Docker build') {
+            stage('Docker build and publish') {
                 steps {
                     sh 'git clone -b master https://github.com/rbera04/docker.git'
                     sh 'cp java.dockerfile myfirstrepo/my-app/'
-                    sh 'cd myfirstrepo/my-app && docker build -t myapp:v1 .'
+                    sh 'cd myfirstrepo/my-app && docker build -t rbera08/myapp:v1 .'
+                    sh 'docker push rbera08/myapp:v1'
                 }
             }
             stage('deploy'){
                 steps {
+                    sh 'docker pull rbera08/myapp:v1'
                     sh 'docker run -dit -p 8085:80 myapp:v1'
                 }
             }
